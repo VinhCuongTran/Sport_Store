980c4941e719257001549eb10b582b0c317a4736
VinhCuong <vinhcuong304@gmail.com>
2026-02-26 15:53:41 +0700

    Khởi chạy dự án lần đầu tiên

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const app = require("./app");
+const config = require("./app/config");
+
+// Start the server
+const PORT = config.app.port;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Sport_Marketplace
+# SPORT_MARKETPLACE
+
+Học kỳ 2, Năm học 2025-2026
+
+**MSSV**: B2205856
+
+**Họ tên SV**: Trần Vĩnh Cường
